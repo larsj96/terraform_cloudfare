965790c71bc59a214fe740007d6e2598c9efb6f4
Lars Jørgen# <ljn@aztek.no>
2022-10-24 15:35:39 +0200

    cloudfare DNS

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "3.26.0"
+    }
+  }
+
+    backend "http" {
+  }
+}
+
+
+
+data "cloudflare_record" "test" {
+  zone_id  = "37b50951304d33118935e0fcfe56f04c"
+  hostname = "lanilsen.xyz"
+}
