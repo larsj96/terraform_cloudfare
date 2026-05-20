250ea7ffeb73eb36e81c149ad4ae795bb2499593
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-20 23:15:56 +0200

    Add docs tunnel variables

---
+++ docs-tunnel/variables.tf
@@ -0,0 +1,36 @@
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token with DNS edit and Cloudflare Tunnel write permissions."
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID."
+  type        = string
+  default     = "10fe772bf2d6ab4c5ec76c0aceba8ae6"
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID for lanilsen.com."
+  type        = string
+  default     = "37b50951304d33118935e0fcfe56f04c"
+}
+
+variable "docs_hostname" {
+  description = "Public docs hostname."
+  type        = string
+  default     = "docs.lanilsen.com"
+}
+
+variable "docs_origin_url" {
+  description = "Internal origin URL reached from cloudflared."
+  type        = string
+  default     = "http://10.0.0.37"
+}
+
+variable "tunnel_name" {
+  description = "Cloudflare Tunnel name."
+  type        = string
+  default     = "homelab-docs"
+}
