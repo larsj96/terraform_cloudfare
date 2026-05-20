24a7be1b560fe1d5abd1dbca64107f534132df3c
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-21 00:16:46 +0200

    Output docs Access application id

--- docs-tunnel/outputs.tf
+++ docs-tunnel/outputs.tf
@@ -6,6 +6,10 @@ output "docs_tunnel_id" {
   value = cloudflare_zero_trust_tunnel_cloudflared.docs.id
 }
 
+output "docs_access_application_id" {
+  value = cloudflare_zero_trust_access_application.docs.id
+}
+
 output "cloudflared_tunnel_token" {
   value     = data.cloudflare_zero_trust_tunnel_cloudflared_token.docs.token
   sensitive = true
