75c6e389a1b9302a54643230ac64ebe85152692f
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-21 00:19:43 +0200

    Output Access identity provider id

--- docs-tunnel/outputs.tf
+++ docs-tunnel/outputs.tf
@@ -10,6 +10,10 @@ output "docs_access_application_id" {
   value = cloudflare_zero_trust_access_application.docs.id
 }
 
+output "docs_access_identity_provider_id" {
+  value = cloudflare_zero_trust_access_identity_provider.onetimepin.id
+}
+
 output "cloudflared_tunnel_token" {
   value     = data.cloudflare_zero_trust_tunnel_cloudflared_token.docs.token
   sensitive = true
