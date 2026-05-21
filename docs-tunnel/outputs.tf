cf735421104f48733b60d719e69388d97ad89ae9
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-21 10:08:20 +0200

    Add Grafana tunnel outputs

--- docs-tunnel/outputs.tf
+++ docs-tunnel/outputs.tf
@@ -2,12 +2,20 @@ output "docs_hostname" {
   value = var.docs_hostname
 }
 
+output "grafana_hostname" {
+  value = var.grafana_hostname
+}
+
 output "docs_tunnel_id" {
   value = cloudflare_zero_trust_tunnel_cloudflared.docs.id
 }
 
 output "docs_access_application_id" {
   value = cloudflare_zero_trust_access_application.docs.id
+}
+
+output "grafana_access_application_id" {
+  value = cloudflare_zero_trust_access_application.grafana.id
 }
 
 output "docs_access_identity_provider_id" {
