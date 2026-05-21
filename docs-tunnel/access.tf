be7264a533cc3628c9ba3f0670665304091b7adb
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-21 10:08:21 +0200

    Add Grafana Cloudflare Access app

--- docs-tunnel/access.tf
+++ docs-tunnel/access.tf
@@ -27,3 +27,28 @@ resource "cloudflare_zero_trust_access_application" "docs" {
     ]
   }]
 }
+
+resource "cloudflare_zero_trust_access_application" "grafana" {
+  account_id           = var.cloudflare_account_id
+  name                 = "Homelab Grafana"
+  domain               = var.grafana_hostname
+  type                 = "self_hosted"
+  session_duration     = "24h"
+  app_launcher_visible = false
+  allowed_idps         = [cloudflare_zero_trust_access_identity_provider.onetimepin.id]
+
+  policies = [{
+    name             = "Allow Lars Grafana access"
+    decision         = "allow"
+    session_duration = "24h"
+    precedence       = 1
+
+    include = [
+      {
+        email = {
+          email = "larsj96@gmail.com"
+        }
+      },
+    ]
+  }]
+}
